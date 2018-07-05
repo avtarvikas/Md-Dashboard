a8d3e8d0f35d882fc7e06da15360750229539932
avtarvikas <avtarvikas236@gmail.com>
2018-07-05 16:01:03 +0530

    setting up DND

--- src/containers/DndDashboard.js
+++ src/containers/DndDashboard.js
@@ -5,6 +5,13 @@ import { Container } from "mdbreact";
 import { data, dataLine2 } from "../constants";
 import { Header, Sidebar, UserActivity } from "../components";
 
+const widget = [
+  { id: 1, name: UserActivity },
+  { id: 2, name: UserActivity },
+  { id: 3, name: UserActivity },
+  { id: 4, name: UserActivity }
+];
+
 class DndDashboard extends Component {
   constructor(props) {
     super(props);
@@ -45,217 +52,11 @@ class DndDashboard extends Component {
               </div>
             </div>
             <div className="row m0">
-              <div className="col-4 p0">
-                <UserActivity />
-
-                <Card className="genericCard">
-                  <CardBody className="border-bottom">
-                    <h2 className="genericCardHTag"> Resources </h2>
-                  </CardBody>
-                  <CardBody className="border-bottom">
-                    <h2 className="genericCardHTagChild"> Compute </h2>
-                    <div className="subtitle-span">
-                      <i className="fa fa-sun-o iconsSize" />
-                      <span>App Engine</span>
-                      <br />
-                      <span className="subtitle-span-child">0 instances</span>
-                    </div>
-                    <div className="subtitle-span">
-                      <i className="fa fa-microchip iconsSize" />
-                      <span>Compute Engine</span>
-                      <br />
-                      <span className="subtitle-span-child">1 instances</span>
-                    </div>
-                  </CardBody>
-                  <CardBody className="border-bottom">
-                    <h2 className="genericCardHTagChild"> Stroage </h2>
-                    <div className="subtitle-span">
-                      <i className="fa fa-archive iconsSize" />
-                      <span>Cloud Stroage</span>
-                      <br />
-                      <span className="subtitle-span-child">2 buckets</span>
-                    </div>
-                  </CardBody>
-                  <CardBody className="border-bottom">
-                    <h2 className="genericCardHTagChild"> Big Data </h2>
-                    <div className="subtitle-span">
-                      <i className="fa fa-database iconsSize" />
-                      <span> BigQuery </span>
-                      <br />
-                      <span className="subtitle-span-child">
-                        Data unavailable
-                      </span>
-                    </div>
-                  </CardBody>
-                </Card>
-
-                <Card className="genericCard">
-                  <CardBody className="border-bottom">
-                    <h2 className="genericCardHTag">Explore other services</h2>
-                  </CardBody>
-                  <CardBody className="border-bottom">
-                    <div className="subtitle-span">
-                      <i className="fa fa-codepen iconsSize" />
-                      <span>Enable APIS and get credentials like keys</span>
-                      <br />
-                    </div>
-                  </CardBody>
-                  <CardBody className="border-bottom">
-                    <div className="subtitle-span">
-                      <i className="fa fa-archive iconsSize" />
-                      <span>Deploy and prebuild soultions</span>
-                      <br />
-                    </div>
-                  </CardBody>
-                  <CardBody className="border-bottom">
-                    <div className="subtitle-span">
-                      <i className="fa fa-database iconsSize" />
-                      <span>Try new cloud debugger </span>
-                      <br />
-                    </div>
-                  </CardBody>
-                </Card>
-              </div>
-              <div className="col-4 p0">
-                <Card className="genericCard">
-                  <CardBody className="border-bottom">
-                    <h2 className="genericCardHTag"> App Engine </h2>
-                  </CardBody>
-                  <CardBody className="border-bottom">
-                    <h2 className="genericCardHTagChild">
-                      Summary (count/sec)
-                    </h2>
-                    <Container>
-                      <Line data={data} />
-                    </Container>
-                    <i
-                      className="fa fa-square bluetint"
-                      aria-hidden="true"
-                      style={{ marginRight: "5px" }}
-                    />
-                    <span className="genericCardHTagChild">
-                      Requests : 0.0333
-                    </span>
-                  </CardBody>
-                </Card>
-
-                <Card className="genericCard">
-                  <CardBody className="border-bottom">
-                    <h2 className="genericCardHTag"> APIs </h2>
-                  </CardBody>
-                  <CardBody className="border-bottom">
-                    <h2 className="genericCardHTagChild">
-                      Requests (request/sec)
-                    </h2>
-                    <Container>
-                      <Line data={dataLine2} />
-                    </Container>
-                    <i
-                      className="fa fa-square bluetint"
-                      aria-hidden="true"
-                      style={{ marginRight: "5px" }}
-                    />
-                    <span className="genericCardHTagChild">
-                      Requests : 0.0267
-                    </span>
-                  </CardBody>
-                </Card>
-              </div>
-              <div className="col-4 p0">
-                <Card className="genericCard">
-                  <CardBody className="border-bottom">
-                    <h2 className="genericCardHTag">Google Platform Status</h2>
-                  </CardBody>
-                  <CardBody className="border-bottom">
-                    <div className="margin10">
-                      <i
-                        className="fa fa-check-circle greenCircle"
-                        aria-hidden="true"
-                      />
-                      <span> All services normal </span>
-                    </div>
-                  </CardBody>
-                </Card>
-
-                <Card className="genericCard">
-                  <CardBody className="border-bottom">
-                    <h2 className="genericCardHTag"> Billing </h2>
-                  </CardBody>
-                  <CardBody className="border-bottom">
-                    <h2 className="medium-text-black"> $0.0 </h2>
-                    <h2 className="medium-text">
-                      Approximate charges so far this month
-                    </h2>
-                  </CardBody>
-                </Card>
-
-                <Card className="genericCard">
-                  <CardBody className="border-bottom">
-                    <h2 className="genericCardHTag"> News </h2>
-                  </CardBody>
-                  <CardBody className="border-bottom">
-                    <h2 className="medium-text-black">
-                      Google shares software network load balancers design
-                      powering GCP networking
-                    </h2>
-                    <h2 className="medium-text">
-                      Google cloud platform blog - 4 days ago
-                    </h2>
-                  </CardBody>
-                  <CardBody className="border-bottom">
-                    <h2 className="medium-text-black">
-                      Google coumputes engine boosts high availablity controls
-                    </h2>
-                    <h2 className="medium-text">
-                      Google cloud platform blog - 4 days ago
-                    </h2>
-                  </CardBody>
-                  <CardBody className="border-bottom">
-                    <h2 className="medium-text-black">
-                      Calculating and searching 500 billing digits of Pi
-                    </h2>
-                    <h2 className="medium-text">
-                      Google cloud platform blog - 6 days ago
-                    </h2>
-                  </CardBody>
-                </Card>
-
-                <Card className="genericCard">
-                  <CardBody className="border-bottom">
-                    <h2 className="genericCardHTag"> Documentation </h2>
-                  </CardBody>
-                  <CardBody className="border-bottom">
-                    <div className="subtitle-span">
-                      <i className="fa fa-file iconsSize" />
-                      <span className="medium-text-black">
-                        Google Cloud Platform Documentation
-                      </span>
-                      <i className="fa fa-external-link-square iconsSize margin-left-4" />
-                      <br />
-                    </div>
-                  </CardBody>
-                  <CardBody className="border-bottom">
-                    <div className="subtitle-span">
-                      <i className="fa fa-file iconsSize" />
-                      <span className="medium-text-black">
-                        Cloud Platform solutions
-                      </span>
-                      <i className="fa fa-external-link-square iconsSize margin-left-4" />
-                      <br />
-                    </div>
-                  </CardBody>
-                  <CardBody className="border-bottom">
-                    <div className="subtitle-span">
-                      <i className="fa fa-file iconsSize" />
-                      <span className="medium-text-black">
-                        Cloud Platform solutions
-                      </span>
-                      <i className="fa fa-external-link-square iconsSize margin-left-4" />
-                      <br />
-                    </div>
-                  </CardBody>
-                </Card>
-              </div>
+              {widget.map(obj => (
+                <div className="col-4 p0" key={obj.id}>
+                  <UserActivity />
+                </div>
+              ))}
             </div>
           </div>
         </div>
